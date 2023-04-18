ff8c85d12879589204f409326b3af27fa598d189
cheesePizza2 <34486300+cheesePizza2@users.noreply.github.com>
2023-04-18 09:40:29 -0400

    i fucking hate tgui

--- tgui/packages/tgui/interfaces/PersonalGoals.js
+++ tgui/packages/tgui/interfaces/PersonalGoals.js
@@ -2,9 +2,17 @@ import { Window } from '../layouts';
 import { Section } from '../components';
 
 export const PersonalGoals = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { goals = [] } = data;
   <Window title="Personal Goals" width={675} height={700}>
     <Window.Content scrollable>
-      <Section />
+      <Section>
+        <Table>
+          {goals.map(goal) => (
+
+          )}
+        </Table>
+      </Section>
     </Window.Content>
   </Window>;
 };
