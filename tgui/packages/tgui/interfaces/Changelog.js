a238005ffacce4955ad2abd3441ad93818770ac2
TheDarkElites <73414180+TheDarkElites@users.noreply.github.com>
2023-08-07 14:39:26 -0400

    Adds in our favorite lore hater
    (I forgot the first time)

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -206,7 +206,7 @@ export class Changelog extends Component {
         <p>
           <b>Coders: </b>
           Lestatanderson, Tsurupeta, HarryOB, ivanmixo, Autisme, DTraitor,
-          TheDarkElites, cheesePizza2, Seven
+          TheDarkElites, cheesePizza2, Seven, EgorDinamit
         </p>
         <p>
           <b>Spriters: </b>
