5d85ad624b4dff9b6f4a80502454f489bad5825f
cheesePizza2 <34486300+cheesePizza2@users.noreply.github.com>
2024-04-02 11:21:33 -0400

    unit test lowered wooo

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -48,7 +48,7 @@ exactly 0 "density = 0/1" 'density\s*=\s*\d' -P
 exactly 0 "emagged = 0/1" 'emagged\s*=\s*\d' -P
 exactly 0 "simulated = 0/1" 'simulated\s*=\s*\d' -P
 exactly 2 ".proc/ uses" '([, ])\.proc/(\w+)'
-exactly 11 "/proc/ uses" '([(, ])/proc/(\w+)'
+exactly 9 "/proc/ uses" '([(, ])/proc/(\w+)'
 # With the potential exception of << if you increase any of these numbers you're probably doing it wrong
 # - Fuck you, I am going to use as many text2path as I want
 
